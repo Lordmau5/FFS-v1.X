ea15fa61db16708e0f230e6938711e9dd3c3c061
Max Gurela <maxpowa1@gmail.com>
2015-07-13 10:35:26 -0700

    Add Google Analytics tracking

--- js/ffs.js
+++ js/ffs.js
@@ -13,3 +13,12 @@ $(window).load(function() {
 /* favicon.js http://mit-license.org */
 (function(h,e){if(!h.favicon){var f=e.getElementsByTagName("head")[0],d=null,g=function(a){var b=e.createElement("link");b.type="image/x-icon";b.rel="icon";b.href=a;a=f.getElementsByTagName("link");for(var c=a.length;0<=--c;/\bicon\b/i.test(a[c].getAttribute("rel"))&&f.removeChild(a[c]));f.appendChild(b)};h.favicon={defaultPause:2E3,change:function(a,b){clearTimeout(d);b&&(e.title=b);""!==a&&g(a)},animate:function(a,b){clearTimeout(d);a.forEach(function(a){(new Image).src=a});b=b||this.defaultPause;
 var c=0;g(a[c]);d=setTimeout(function k(){c=(c+1)%a.length;g(a[c]);d=setTimeout(k,b)},b)},stopAnimate:function(){clearTimeout(d)}}}})(this,document);
+
+/* Google Analytics */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-65076556-1', 'auto');
+ga('send', 'pageview');
